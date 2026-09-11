27fee0267c509d09d7b35117532a48c45bcec033
Ands081 <146835521+Ands081@users.noreply.github.com>
2026-09-11 12:06:38 +0200

    Update interval.hs

--- my-ebpf-cfg/src_mini/interval.hs
+++ my-ebpf-cfg/src_mini/interval.hs
@@ -4,7 +4,22 @@ data Bound
     | PosInf
     deriving (Show, Eq)
 
+instance Ord Bound where
+    compare NegInf NegInf = EQ
+    compare NegInf _      = LT
+    compare PosInf PosInf = EQ
+    compare PosInf _      = GT
+    compare (Finite x) (Finite y) = compare x y
+    compare (Finite _) PosInf = LT
+    compare (Finite _) NegInf = GT
+
 data Interval
     = Bottom
     | Interval Bound Bound
     deriving (Show, Eq)
+
+instance Ord Interval where
+    Bottom <= _ = True
+    _ <= Bottom = False
+    Interval l1 u1 <= Interval l2 u2 =
+        l2 <= l1 && u1 <= u2
