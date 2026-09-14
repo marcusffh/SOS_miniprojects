be7e1034dc73a27250ce25cad3aa7e2b41990106
Ands081 <146835521+Ands081@users.noreply.github.com>
2026-09-14 14:26:15 +0200

    Update Environment.hs

--- my-ebpf-cfg/src_mini/Environment.hs
+++ my-ebpf-cfg/src_mini/Environment.hs
@@ -1,6 +1,7 @@
 module Environment where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
 import Interval
 import Ebpf.Asm (Reg)
 
@@ -11,4 +12,18 @@ type Memory = Map Int Interval
 -- Take a memory adress such as 0 or 25, and map it to an interval
 
 data State = State Environment Memory
+    deriving (Show, Eq)
 -- At each program point, our analysis state consist of both the register environment and the memory
+
+joinEnvironment :: Environment -> Environment -> Environment
+joinEnvironment =
+    Map.unionWith joinInterval
+
+joinMemory :: Memory -> Memory -> Memory
+joinMemory =
+    Map.unionWith joinInterval
+
+joinState :: State -> State -> State
+joinState (State env1 mem1) (State env2 mem2) =
+    State (joinEnvironment env1 env2)
+          (joinMemory mem1 mem2)
